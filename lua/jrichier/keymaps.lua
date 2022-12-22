28c8f909e776d8ee65f4adf74584cb28aa1f2ad7
Jason Richier <jason.richier@soprasteria.com>
2022-12-23 00:04:00 +0100

    add: keymaps

---
+++ lua/jrichier/keymaps.lua
@@ -0,0 +1,18 @@
+-- windows movement 
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = 'Go to left window'})
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = 'Go to down window'})
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = 'Go to up window'})
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = 'Go to right window'})
+
+vim.keymap.set("n", "<leader>ws", "<C-w>s<C-w>j", { desc = 'Split horitonally the current window' })
+vim.keymap.set("n", "<leader>wv", "<C-w>v<C-w>l", { desc = 'Split vertically the current window' })
+vim.keymap.set("n", "<leader>wo", "<C-w>o", { desc = 'Make the current window the main one' })
+vim.keymap.set("n", "<leader>wc", "<C-w>c", { desc = 'Close the current window' })
+
+-- Stay in indent mode
+vim.keymap.set("v", "<", "<gv", {})
+vim.keymap.set("v", ">", ">gv", {})
+
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<cr>")
