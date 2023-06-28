4b764076132b3e62e537702dae6879a71b378f98
Jason Richier <jason.richier@soprasteria.com>
2023-06-28 11:08:27 +0200

    feat(editor): add flash.nvim own config

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,5 +1,28 @@
 return {
   {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {},
+    keys = {
+      {
+        "s",
+        mode = { "n", "x", "o" },
+        function()
+          -- default options: exact mode, multi window, all directions, with a backdrop
+          require("flash").jump({
+            search = {
+              mode = function(str)
+                return "\\<" .. str
+              end,
+            },
+          })
+        end,
+        desc = "Flash",
+      },
+    },
+  },
+  {
     "nvim-telescope/telescope.nvim",
     keys = {
       -- add a keymap to browse plugin files
@@ -47,23 +70,6 @@ return {
   { "folke/noice.nvim", enabled = false },
   { "stevearc/dressing.nvim", enabled = false },
   { "akinsho/bufferline.nvim", opts = { options = { mode = "tabs" } } },
-  -- {
-  --   "nvim-lualine/lualine.nvim",
-  --   opts = {
-  --     options = {
-  --       disabled_filetypes = { "neo-tree", "alpha" },
-  --       globalstatus = false,
-  --       inactive_sections = {
-  --         lualine_a = {},
-  --         lualine_b = {},
-  --         lualine_c = { "filename" },
-  --         lualine_x = { "location" },
-  --         lualine_y = {},
-  --         lualine_z = {},
-  --       },
-  --     },
-  --   },
-  -- },
 
   -- Neorg
   {
