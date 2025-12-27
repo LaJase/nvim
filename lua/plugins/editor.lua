da9e0ec2b7cabc0aa03bb508e86c9ffa6d46cdea
Jason Richier <jason.richier@soprasteria.com>
2025-12-27 23:46:15 +0100

    chore: more updates trying lazyvim distro

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -13,7 +13,15 @@ return {
       colorscheme = "onedark",
     },
   },
-
+  {
+    "folke/snacks.nvim",
+    opts = {
+      zen = { enabled = false },
+    },
+    keys = {
+      { "<leader>uz", false },
+    },
+  },
   {
     "folke/flash.nvim",
     event = "VeryLazy",
@@ -38,30 +46,9 @@ return {
     },
   },
   {
-    "nvim-telescope/telescope.nvim",
-    keys = {
-      -- add a keymap to browse plugin files
-      -- stylua: ignore
-      {
-        "<leader>fp",
-        function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
-        desc = "Find Plugin File",
-      },
-    },
-  },
-  {
     "NeogitOrg/neogit",
     cmd = { "Neogit" },
     keys = {
-      -- stylua: ignore
-      {
-        "<C-p>", function()
-          local builtin = require('telescope.builtin')
-          local telescope_themes = require('telescope.themes')
-          builtin.find_files(telescope_themes.get_dropdown({ previewer = false, }))
-        end,
-        { desc = "Find files in all project's files" },
-      },
       -- stylua: ignore
       {
         "<leader>gn", function() vim.cmd("Neogit") end, desc = "Neogit (tab)",
